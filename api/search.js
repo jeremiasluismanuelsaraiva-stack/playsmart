dc8af7700249338767da7c7ccaf6c78eb0b8211f
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-06-20 15:16:38 +0200

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,61 +1,103 @@
-
-
-
 const fetch = require("node-fetch");
 
-const API_CYBERHOST="https://api.cyberhost.online";
+
+const API_CYBERHOST = "https://api.cyberhost.online";
+
 const API_KEY_CYBERHOST = "cyber_f857ee31300990f3451d1a6826f9913b74d52f0a";
 
 
-module.exports = async(req,res)=>{
+module.exports = async (req,res)=>{
+
+
+res.setHeader(
+"Content-Type",
+"application/json"
+);
+
 
 try{
 
 
-const q=req.query.q;
+const q = req.query.q;
 
 
-const r=await fetch(
+if(!q){
+
+return res.json([]);
+
+}
+
+
+
+const r = await fetch(
+
 `${API_CYBERHOST}/youtube/search`,
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
 
-api_key:API_KEY_CYBERHOST,
-query:q,
-limit:10
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":"application/json"
+
+},
+
+body: JSON.stringify({
+
+api_key: API_KEY_CYBERHOST,
+
+query: q,
+
+limit: 10
 
 })
-});
+
+}
+
+);
 
 
-const data=await r.json();
+
+const data = await r.json();
 
 
-const lista=data.results || data.data || [];
+
+const lista =
+data.results ||
+data.data ||
+[];
 
 
-const resposta=lista.map(x=>({
+
+const resposta = lista.map(x=>({
+
 
 nome:
-x.title || x.name,
+x.title ||
+x.name ||
+"Sem título",
+
 
 
 artista:
-x.channel || "YouTube",
+x.channel ||
+x.artist ||
+"YouTube",
 
 
-// AQUI é importante
+
 url:
 x.url ||
 x.videoUrl ||
 x.link ||
-x.webpage_url
+x.webpage_url ||
+x.video
+
+
 
 }));
+
 
 
 res.json(resposta);
@@ -64,9 +106,13 @@ res.json(resposta);
 
 }catch(e){
 
+
 res.status(500).json({
+
 erro:e.message
+
 });
+
 
 }
 
