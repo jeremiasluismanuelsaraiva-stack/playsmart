c3eefccda7bfe941064f5418230ccf1c0c90294e
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-06-20 15:11:23 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,16 +4,20 @@ console.log("SCRIPT CARREGADO");
 let musicas = [];
 let atual = 0;
 
+
 const lista = document.getElementById("lista");
 const audio = document.getElementById("audio");
 const titulo = document.getElementById("titulo");
+
 
 
 function render(){
 
 lista.innerHTML = "";
 
+
 musicas.forEach((m,i)=>{
+
 
 lista.innerHTML += `
 
@@ -23,13 +27,20 @@ lista.innerHTML += `
 
 <p>🎤 ${m.artista}</p>
 
+
 <button onclick="tocar(${i})">
+
 ▶️ Escutar
+
 </button>
 
+
 <button onclick="verVideo(${i})">
+
 🎬 Ver
+
 </button>
+
 
 </div>
 
@@ -37,50 +48,65 @@ lista.innerHTML += `
 
 });
 
+
 }
 
 
 
-function tocar(i){
 
-atual = i;
 
-audio.src = musicas[i].audio;
+async function tocar(i){
+
+
+atual=i;
+
 
 titulo.innerHTML =
-"🎵 " + musicas[i].nome;
+"⏳ Preparando "+musicas[i].nome;
+
+
+
+const res = await fetch(
+
+"/api/baixar?url="+
+encodeURIComponent(musicas[i].url)+
+"&tipo=audio"
+
+);
+
+
+
+const data = await res.json();
+
+
+
+if(!data.sucesso){
+
+alert(data.erro);
+
+return;
+
+}
+
+
+
+audio.src = data.download;
+
+
+titulo.innerHTML =
+
+"🎵 "+musicas[i].nome+
+"<br>🎤 "+musicas[i].artista;
+
+
 
 audio.play();
 
-}
-
-
-
-function proxima(){
-
-if(atual < musicas.length-1){
-
-atual++;
-
-tocar(atual);
-
-}
 
 }
 
 
 
-function anterior(){
-
-if(atual > 0){
-
-atual--;
-
-tocar(atual);
-
-}
-
-}
 
 
 
@@ -94,7 +120,7 @@ document.getElementById("busca").value.trim();
 
 if(!q){
 
-alert("Digite uma música");
+alert("Digite artista ou música");
 
 return;
 
@@ -103,22 +129,18 @@ return;
 
 
 lista.innerHTML =
-"⏳ A carregar...";
+"⏳ Pesquisando...";
 
 
 
 try{
 
 
-// usa link do youtube na pesquisa
-const url = q;
-
-
-
 const res = await fetch(
-"/api/baixar?url=" +
-encodeURIComponent(url) +
-"&tipo=audio"
+
+"/api/search?q="+
+encodeURIComponent(q)
+
 );
 
 
@@ -127,31 +149,18 @@ const data = await res.json();
 
 
 
-if(!data.sucesso){
+if(!data.length){
 
-throw new Error(data.erro);
+throw new Error("Nada encontrado");
 
 }
 
 
 
-musicas.push({
-
-nome:q,
-
-artista:"YouTube",
-
-audio:data.download
-
-});
-
+musicas = data;
 
 
 render();
-
-
-
-tocar(musicas.length-1);
 
 
 
@@ -169,16 +178,50 @@ lista.innerHTML =
 
 
 
-function verVideo(i){
 
 
-window.open(
-musicas[i].audio,
-"_blank"
-);
+function proxima(){
+
+
+if(atual < musicas.length-1){
+
+atual++;
+
+tocar(atual);
+
+}
 
 
 }
 
 
-render();
+
+
+function anterior(){
+
+
+if(atual > 0){
+
+atual--;
+
+tocar(atual);
+
+}
+
+
+}
+
+
+
+
+
+function verVideo(i){
+
+
+window.open(
+musicas[i].url,
+"_blank"
+);
+
+
+}
