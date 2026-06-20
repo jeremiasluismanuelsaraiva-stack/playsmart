858d1741e4aa1eeca7d5073c9c36853b602ce462
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-06-20 15:32:33 +0200

    Update baixar.js

--- api/baixar.js
+++ api/baixar.js
@@ -1,119 +1,55 @@
-const https = require("https");
 
-const API_CYBERHOST = "https://api.cyberhost.online";
-const API_KEY_CYBERHOST = "cyber_f857ee31300990f3451d1a6826f9913b74d52f0a";
+const fetch=require("node-fetch");
 
 
-function postCyber(body){
+const API="https://api.cyberhost.online";
 
-return new Promise((resolve,reject)=>{
-
-
-const data = JSON.stringify(body);
+const KEY="cyber_f857ee31300990f3451d1a6826f9913b74d52f0a";
 
 
-const req = https.request(
-API_CYBERHOST + "/youtube/download",
+
+module.exports=async(req,res)=>{
+
+
+try{
+
+
+let {url,tipo}=req.query;
+
+
+
+let r=await fetch(
+
+API+"/youtube/download",
+
 {
+
 method:"POST",
+
 headers:{
-"Content-Type":"application/json",
-"Content-Length":Buffer.byteLength(data)
-}
+
+"Content-Type":"application/json"
+
 },
-(res)=>{
 
-let result="";
+body:JSON.stringify({
 
-
-res.on("data",chunk=>{
-result += chunk;
-});
-
-
-res.on("end",()=>{
-
-try{
-
-resolve(JSON.parse(result));
-
-}catch(e){
-
-reject(new Error(result));
-
-}
-
-});
-
-});
-
-
-req.on("error",reject);
-
-
-req.write(data);
-
-req.end();
-
-
-});
-
-}
-
-
-
-module.exports = async (req,res)=>{
-
-
-res.setHeader(
-"Content-Type",
-"application/json"
-);
-
-
-
-try{
-
-
-const url = req.query.url;
-
-const tipo = req.query.tipo || "audio";
-
-
-if(!url){
-
-return res.json({
-sucesso:false,
-erro:"Sem URL"
-});
-
-}
-
-
-
-const data = await postCyber({
-
-api_key:API_KEY_CYBERHOST,
+api_key:KEY,
 
 url:url,
 
-type:
-tipo==="video"
-?
-"video"
-:
-"audio",
+type:tipo==="video"?"video":"audio",
 
-format:
-tipo==="video"
-?
-"mp4"
-:
-"mp3",
+format:tipo==="video"?"mp4":"mp3",
 
 quality:"720"
 
+})
+
 });
+
+
+let data=await r.json();
 
 
 
@@ -121,11 +57,7 @@ if(!data.file){
 
 return res.json({
 
-sucesso:false,
-
-erro:"CyberHost sem ficheiro",
-
-resposta:data
+erro:"CyberHost sem ficheiro"
 
 });
 
@@ -133,23 +65,15 @@ resposta:data
 
 
 
-let link=data.file;
-
-
-if(!link.startsWith("http")){
-
-link =
-API_CYBERHOST +
-"/youtube" +
-link;
-
-}
+let link=data.file.startsWith("http")
+?
+data.file
+:
+API+"/youtube"+data.file;
 
 
 
-return res.json({
-
-sucesso:true,
+res.json({
 
 download:link
 
@@ -160,9 +84,7 @@ download:link
 }catch(e){
 
 
-return res.status(500).json({
-
-sucesso:false,
+res.json({
 
 erro:e.message
 
@@ -171,4 +93,4 @@ erro:e.message
 }
 
 
-};
+}
